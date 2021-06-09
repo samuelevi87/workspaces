2d5fb955f17ddf57a41413581f2777437ae29190
samuelevi87 <samuel.levi.alves@outlook.com>
2021-06-09 20:55:21 -0300

    Resolução com 04 discos.

--- atividade02/src/TorreHanoi04Discos.java
+++ atividade02/src/TorreHanoi04Discos.java
@@ -1,5 +1,27 @@
 public class TorreHanoi04Discos {
     public static void main(String[] args) {
+        String d1 = " Disco 01 (Rosa) ";
+        String d2 = " Disco 02 (Lilás) ";
+        String d3 = " Disco 03 (Roxo) ";
+        String d4 = " Disco 04 (Azul) ";
+        String t1 = " Torre 01 (Esquerda) ";
+        String t2 = " Torre 02 (Centro) ";
+        String t3 = " Torre 03 (Direita) ";
 
+        System.out.println("Mover o disco" + d1 + "para a" + t2);
+        System.out.println("Mover o disco" + d2 + "para a" + t3);
+        System.out.println("Mover o disco" + d1 + "para a" + t3);
+        System.out.println("Mover o disco" + d3 + "para a" + t2);
+        System.out.println("Mover o disco" + d1 + "para a" + t1);
+        System.out.println("Mover o disco" + d2 + "para a" + t2);
+        System.out.println("Mover o disco" + d1 + "para a" + t2);
+        System.out.println("Mover o disco" + d4 + "para a" + t3);
+        System.out.println("Mover o disco" + d1 + "para a" + t3);
+        System.out.println("Mover o disco" + d2 + "para a" + t1);
+        System.out.println("Mover o disco" + d1 + "para a" + t1);
+        System.out.println("Mover o disco" + d3 + "para a" + t3);
+        System.out.println("Mover o disco" + d1 + "para a" + t2);
+        System.out.println("Mover o disco" + d2 + "para a" + t3);
+        System.out.println("Mover o disco" + d1 + "para a" + t3);
     }
 }
