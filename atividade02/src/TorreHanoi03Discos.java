7f601bb9aceb6eefaa220a9f7d7a178a3fc95034
samuelevi87 <samuel.levi.alves@outlook.com>
2021-06-09 20:29:50 -0300

    Resolução com 03 discos.

--- atividade02/src/TorreHanoi03Discos.java
+++ atividade02/src/TorreHanoi03Discos.java
@@ -1,5 +1,12 @@
 public class TorreHanoi03Discos {
     public static void main(String[] args) {
 
+        System.out.println("Mover o disco rosa para a torre 03");
+        System.out.println("Mover o disco lilás para a torre 02");
+        System.out.println("Mover o disco rosa para a torre 02");
+        System.out.println("Mover o disco roxo para a torre 03");
+        System.out.println("Mover o disco rosa para a torre 01");
+        System.out.println("Mover o disco lilás para a torre 03");
+        System.out.println("Mover o disco rosa para a torre 03");
     }
 }
